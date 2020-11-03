3b1d5bc467e389ed598f84e068db815a77214bab
Wisketchy Dobrov <warfolomey@gmail.com>
2020-11-03 20:48:46 +0300

    Create some_things_for_colab.py

---
+++ some_things_for_colab.py
@@ -0,0 +1,16 @@
+###########################
+'''
+Shows a video in colab
+'''
+
+from IPython.display import HTML
+from base64 import b64encode
+mp4 = open(movie_name,'rb').read()
+data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
+HTML("""
+<video width=400 controls>
+      <source src="%s" type="video/mp4">
+</video>
+""" % data_url)
+
+####################
