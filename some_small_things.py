8f21024d0e87e7c214173f018b91f691d71ed113
Wisketchy Dobrov <warfolomey@gmail.com>
2020-11-10 16:12:16 +0300

    Update some_small_things.py

--- some_small_things.py
+++ some_small_things.py
@@ -38,4 +38,12 @@ with futures.ThreadPoolExecutor(max_workers=1000) as executor:
 
     
  #########################
+'''
+Flattening
+'''
 
+nested_lists = [[1, 2], [[3, 4], [5, 6], [[7, 8], [9, 10], [[11, [12, 13]]]]]]
+flatten = lambda x: [y for l in x for y in flatten(l)] if type(x) is list else [x]
+flatten(nested_lists)
+
+#########################
