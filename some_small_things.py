1659d39b600eadfd6f5ddffa9c17c09201a14e58
Wisketchy Dobrov <warfolomey@gmail.com>
2020-11-10 16:13:48 +0300

    Update some_small_things.py

--- some_small_things.py
+++ some_small_things.py
@@ -47,3 +47,17 @@ flatten = lambda x: [y for l in x for y in flatten(l)] if type(x) is list else [
 flatten(nested_lists)
 
 #########################
+'''
+Timeit decorator
+'''
+
+def timeit(fn): 
+    # *args and **kwargs are to support positional and named arguments of fn
+    def get_time(*args, **kwargs): 
+        start = time.time() 
+        output = fn(*args, **kwargs)
+        print(f"Time taken in {fn.__name__}: {time.time() - start:.7f}")
+        return output  # make sure that the decorator returns the output of fn
+    return get_time
+
+#########################
