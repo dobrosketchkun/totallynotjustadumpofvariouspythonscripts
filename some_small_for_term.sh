99af3e249e4617172f8d33db5f22633dba084a6b
Wisketchy Dobrov <warfolomey@gmail.com>
2021-04-03 23:14:08 +0300

    Update some_small_for_term.sh

--- some_small_for_term.sh
+++ some_small_for_term.sh
@@ -3,3 +3,15 @@
 ls /some/folder/ | wc -l
 
 ##################################
+
+
+
+
+
+
+#####################
+# ffmpeg section    #
+#####################
+
+# adding silent audio
+ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -i input.mp4 -c:v copy -c:a aac -shortest output.mp4
